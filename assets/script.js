b82510d3d8d4cd5c1adfb599e7e74c029c2e207f
Hisham Kader <hisham319@hotmail.com>
2020-10-25 19:10:20 -0400

    added api

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,4 @@
 var cityInput = $("#cityInput");
-
 var isFahrenheit = true;
 var unitSelected;
 var unit = "F";
@@ -23,3 +22,27 @@ $(".units").on("click", "input", function () {
         unit = "C";
     }
 })
+
+$("#submitBtn").on("click", function (e) {
+
+    e.preventDefault();
+
+    city = cityInput.val().trim();
+    setWeather(city)
+});
+
+$("#cityList").on("click", "td", function (e) {
+
+    var clicked = $(this).text();
+    setWeather(clicked);
+});
+
+function setWeather(city) {
+
+    if (isFahrenheit) {
+
+        var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=b774102802580c232f4e227fa165c18f";
+    } else {
+
+        var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=metric&appid=b774102802580c232f4e227fa165c18f";
+    }
