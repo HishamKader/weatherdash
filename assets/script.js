612682b5cefe62ec2a9e52ec32ef462cf855268a
HishamKader <hisham319@hotmial.com>
2024-04-18 19:31:08 -0400

    Big update to the ui and functionality of the website

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,77 @@
 var cityInput = $("#cityInput");
-var isFahrenheit = true;
 var unitSelected;
 var unit = "F";
+var unit2 = "mph";
 
 
+// Check if there's a stored value for the temperature unit preference
+var storedUnit = localStorage.getItem("temperatureUnit");
 
-setWeather("New Jersey");
+// If there's a stored value, use it; otherwise, default to Fahrenheit
+var isFahrenheit = (storedUnit === "C") ? false : true;
 
+
+// Update unit and unit2 based on the initial stored value
+if (!isFahrenheit) {
+    unit = "C";
+    unit2 = "m/s";
+}
 
 $(".units").on("click", "button", function () {
-
     unitSelected = $(this).attr("value");
+
     if (unitSelected === "F˚") {
-
-        isFahrenheit = true;
-
-        unit = "F";
-    } else {
         isFahrenheit = false;
-
+        unit = "F";
+        unit2 = "mph";
+    } else {
+        isFahrenheit = true;
         unit = "C";
+        unit2 = "m/s";
     }
+
+    // Store the selected unit preference in local storage
+    localStorage.setItem("temperatureUnit", isFahrenheit ? "F" : "C");
+});
+
+// Function to switch between Fahrenheit and Celsius
+function toggleTemperatureUnit() {
+    isFahrenheit = !isFahrenheit;
+    if (isFahrenheit) {
+        unit = "F";
+        unit2 = "mph";
+    } else {
+        unit = "C";
+        unit2 = "m/s";
+    }
+    setWeather($("#cityName").text()); // Update weather data with new units
+    // Store the toggled unit preference in local storage
+    localStorage.setItem("temperatureUnit", isFahrenheit ? "F" : "C");
+}
+
+// Attach click event to toggle temperature unit button
+$(".units").on("click", toggleTemperatureUnit);
+
+
+// setWeather("New Jersey");
+
+function getCurrentLocationWeather() {
+    if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var latitude = position.coords.latitude;
+            var longitude = position.coords.longitude;
+            setWeatherByLocation(latitude, longitude);
+        });
+    } else {
+        console.log("Geolocation is not supported by this browser.");
+
+    }
+}
+
+
+
+$("#localBtn").on("click", function (e) {
+    getCurrentLocationWeather();
 })
 
 $("#submitBtn").on("click", function (e) {
@@ -37,7 +88,15 @@ $("#cityList").on("click", "td", function (e) {
     setWeather(clicked);
 });
 
+$(document).ready(function() {
+    var lastCity = localStorage.getItem("lastCityWeather");
+    if (lastCity) {
+        setWeather(lastCity);
+    }
+});
+
 function setWeather(city) {
+
 
     if (isFahrenheit) {
 
@@ -52,25 +111,36 @@ function setWeather(city) {
         method: "GET"
     }).then(function (response) {
 
+
         var lattitude = response.city.coord.lat;
         var longitude = response.city.coord.lon;
 
         getUVI(lattitude, longitude);
 
-        var dateTime = convertDate(response.list[0].dt);
+
+        var longDate = fullDate(response.list[0].dt);
+        var noDec = Math.floor(response.list[0].main.temp);
+        var min = Math.floor(response.list[0].main.temp_min)
+        var max = Math.floor(response.list[0].main.temp_max)
+        var wind = (response.list[0].wind.speed)
+        var weatherDes = (response.list[0].weather[0].description)
 
         $("#cityName").text(response.city.name);
-        $("#temperature").html(response.list[0].main.temp + " &#730" + unit);
+        $("#temperature").html(noDec + "&#730" + unit);
         $("#main_weather").text(response.list[0].weather[0].main)
         $("#humidity").text(response.list[0].main.humidity);
-        $("#wind").text(response.list[0].wind.speed);
+        $("#wind").text(wind + unit2);
         $("#weather_image").attr("src", "http://openweathermap.org/img/w/" + response.list[0].weather[0].icon + ".png");
-        $("#dateString").text(dateTime);
-        $("#feels").html(response.list[0].main.feels_like + " &#730" + unit);
-        $("#max").html(response.list[0].main.temp_max + " &#730" + unit);
-        $("#min").html(response.list[0].main.temp_min + " &#730" + unit);
+        $("#dateString").text(longDate);
+
+        $("#feels").html(response.list[0].main.feels_like + "&#730" + unit);
+        $("#max").html(max + "&#730" + unit);
+        $("#min").html(min + "&#730" + unit);
+        $("#weatherDes").html(weatherDes)
 
         getDailyForecast(city);
+
+        localStorage.setItem("lastCityWeather", city);
 
         function getDailyForecast(city) {
 
@@ -81,26 +151,107 @@ function setWeather(city) {
 
                 $("#forecastDays").empty();
 
+
                 for (var i = 0, j = 1; i < response.list.length, j <= 5; i += 8, j++) {
 
                     var dateTime = convertDate(response.list[i].dt);
+                    var noDec2 = Math.floor(response.list[i].main.temp);
                     var newDiv = $("<div>").attr("id", "day" + j);
-                    var pTemp = $("<p>");
-                    var pHumid = $("<p>");
+                    var pTemp = $("<h4>");
+                    var pHumid = $("<div>");
                     var iconImage = $("<img>");
+                    var des = $("<p>")
+                    var weatherId = (response.list[i].weather[0].main);
 
-                    newDiv.attr("class", "col-lg-2").appendTo("#forecastDays");
+
+                    $("#day1 h6").text("Today");
+                    $(".display-section").attr("id", response.list[0].weather[0].main);
+                    $(".navbar-brand").attr("id", response.list[0].weather[0].main);
+                    newDiv.addClass("col-lg-0").appendTo("#forecastDays");
+                    newDiv.addClass("" + weatherId).appendTo("#forecastDays");
                     newDiv.html("<h6>" + dateTime + "</h6>").appendTo(newDiv);
-                    pTemp.html('<i class="fas fa-thermometer-full"></i>' + response.list[i].main.temp + " &#730" + unit).appendTo(newDiv);
+                    pTemp.html(noDec2 + "&#730" + unit).appendTo(newDiv);
                     pHumid.html('<i class="fas fa-smog"></i>' + response.list[i].main.humidity + "%").appendTo(newDiv);
                     iconImage.attr("src", "http://openweathermap.org/img/w/" + response.list[i].weather[0].icon + ".png").appendTo(newDiv);
+                    des.text(weatherId).appendTo(newDiv);
 
-                    $("#forecastDays").append(newDiv);
                 }
+
             });
         }
     });
 }
+
+
+function setWeatherByLocation(latitude, longitude) {
+
+    var queryURL;
+    if (isFahrenheit) {
+
+        queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&units=imperial&appid=b774102802580c232f4e227fa165c18f";
+    } else {
+
+        queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&units=metric&appid=b774102802580c232f4e227fa165c18f";
+    }
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+
+
+        var lattitude = response.city.coord.lat;
+        var longitude = response.city.coord.lon;
+
+        getUVI(lattitude, longitude);
+
+        var longDate = fullDate(response.list[0].dt);
+        var noDec = Math.floor(response.list[0].main.temp);
+        var min = Math.floor(response.list[0].main.temp_min)
+        var max = Math.floor(response.list[0].main.temp_max)
+        var wind = (response.list[0].wind.speed)
+
+
+        $("#cityName").text(response.city.name);
+        $("#temperature").html(noDec + "&#730" + unit);
+        $("#main_weather").text(response.list[0].weather[0].main)
+        $("#humidity").text(response.list[0].main.humidity);
+        $("#wind").text(wind + unit2);
+        $("#weather_image").attr("src", "http://openweathermap.org/img/w/" + response.list[0].weather[0].icon + ".png");
+        $("#dateString").text(longDate);
+        $("#feels").html(response.list[0].main.feels_like + "&#730" + unit);
+        $("#max").html(max + "&#730" + unit);
+        $("#min").html(min + "&#730" + unit);
+
+        $("#forecastDays").empty();
+
+
+        for (var i = 0, j = 1; i < response.list.length, j <= 5; i += 8, j++) {
+
+            var dateTime = convertDate(response.list[i].dt);
+            var noDec2 = Math.floor(response.list[i].main.temp);
+            var newDiv = $("<div>").attr("id", "day" + j);
+            var pTemp = $("<h4>");
+            var pHumid = $("<div>");
+            var iconImage = $("<img>");
+            var des = $("<p>")
+            var weatherId = (response.list[i].weather[0].main);
+
+
+            $("#day1 h6").text("Today");
+            $(".display-section").attr("id", response.list[0].weather[0].main);
+            $(".navbar-brand").attr("id", response.list[0].weather[0].main);
+            newDiv.addClass("col-lg-0").appendTo("#forecastDays");
+            newDiv.addClass("" + weatherId).appendTo("#forecastDays");
+            newDiv.html("<h6>" + dateTime + "</h6>").appendTo(newDiv);
+            pTemp.html(noDec2 + "&#730" + unit).appendTo(newDiv);
+            pHumid.html('<i class="fas fa-smog"></i>' + response.list[i].main.humidity + "%").appendTo(newDiv);
+            iconImage.attr("src", "http://openweathermap.org/img/w/" + response.list[i].weather[0].icon + ".png").appendTo(newDiv);
+            des.text(weatherId).appendTo(newDiv);
+
+        }
+    });
+}
+
 function getUVI(lattitude, longitude) {
     var queryURL = "https://api.openweathermap.org/data/2.5/uvi?lat=" + lattitude + "&lon=" + longitude + "&appid=b774102802580c232f4e227fa165c18f";
     $.ajax({
@@ -129,49 +280,68 @@ function getUVI(lattitude, longitude) {
             $("#uvindex").addClass("uvi severe")
         }
 
-        
+
     });
 }
 
-// function tempChange() {
-
-// var temp = parseFloat();
-
-
-// $("#temperature").html(temp);
-
-// if (temp > 0 && temp <= 56) {
-
-//     $("#temperature").removeClass();
-//     $("#temperature").addClass("temp cold")
-// }
-// else if (temp > 57 && temp <= 75) {
-
-//     $("#temperature").removeClass();
-//     $("#temperature").addClass("temp warm");
-
-// }
-// else if (temp >= 76 && temp <= 100) {
-
-//     $("#temperature").removeClass();
-//     $("#temperature").addClass("temp hot");
-// }
-// }
-
-function convertDate(date) {
-
-    //  var options = {
-    //         weekday: "long"
-
-    //       };
+function fullDate(date) {
 
     var newDate = new Date(date * 1000);
-    var newDateFormat = newDate.toLocaleDateString();
-
-
-
-    //   console.log(date.toLocaleString("de-DE", options));
+    var newDateFormat = newDate.toDateString();
 
     return newDateFormat;
 }
 
+function convertDate(date) {
+
+    var weekDay = {
+        0: "Sun",
+        1: "Mon",
+        2: "Tue",
+        3: "Wed",
+        4: "Thu",
+        5: "Fri",
+        6: "Sat"
+    }
+    var newDate = new Date(date * 1000);
+    var dayDate = newDate.getDay()
+    var day = weekDay[dayDate]
+
+    return day;
+}
+
+$(document).ready(function () {
+
+    var darkModeEnabled = localStorage.getItem('darkModeEnabled');
+
+    // Apply dark mode if it was enabled previously
+    if (darkModeEnabled === 'true') {
+        $('body').addClass('dark-mode');
+        $('.container h3, .cityPicker td').addClass('dark-mode-text')
+        $('.user-section').addClass('dark-table');
+        $('#submitBtn').addClass('dark-mode-button')
+        $('#cityInput').addClass('dark-mode-input')
+    }
+
+    // Dark mode toggle
+    $('#dark-mode-toggle').click(function () {
+        $('body').addClass('dark-mode');
+        $('.container h3, .cityPicker td').addClass('dark-mode-text')
+        $('.user-section').addClass('dark-table');
+        $('#submitBtn').addClass('dark-mode-button')
+        $('#cityInput').addClass('dark-mode-input')
+
+        localStorage.setItem('darkModeEnabled', 'true');
+    });
+
+    // Light mode toggle
+    $('#light-mode-toggle').click(function () {
+        $('body').removeClass('dark-mode');
+        $('.container h3, .cityPicker td').removeClass('dark-mode-text')
+        $('.user-section').removeClass('dark-table');
+        $('#submitBtn').removeClass('dark-mode-button')
+        $('#cityInput').removeClass('dark-mode-input')
+
+        localStorage.setItem('darkModeEnabled', 'false');
+    });
+});
